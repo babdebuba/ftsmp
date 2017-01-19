887c1bf32344fc7634eadce7fc67e2d82ba08dda
Weigt <t_weig05@WIWI.UNI-MUENSTER.DE>
2017-01-19 07:17:54 +0100

    - cmd check 3 x ok - fitted the line length to less than 65 for high size pc

--- R/model_initialize.R
+++ R/model_initialize.R
@@ -1,6 +1,7 @@
 # model building function for the model yy = zz * beta -----------------------
 model_initialize <- function(yraw, pp, hh,
-                             prior_constant_variance, gg, kk, ll) {
+                             prior_constant_variance,
+                             gg, kk, ll) {
 
   # build the variables of the model
   dd <- dim(yraw)[2] # dimension of the time series
@@ -11,7 +12,8 @@ model_initialize <- function(yraw, pp, hh,
   zz_rows_number <- dim(zz)[2] # number of cols of zz
 
   # build beta_predict_expectation and beta_update_variance
-  beta_update_expectation <- array(0, dim = c(tt, zz_rows_number))
+  beta_update_expectation <- array(0, dim = c(tt,
+    zz_rows_number))
   beta_predict_expectation <- beta_update_expectation
 
   # build beta_predict_variance and beta_update_variance
@@ -20,15 +22,17 @@ model_initialize <- function(yraw, pp, hh,
   beta_predict_variance <- beta_update_variance
 
   # build yy_predict_expectation, yy_predict_variance and yy_update_variance
-  yy_cov <- cov(yy)
+  yy_cov <- stats::cov(yy)
   yy_predict_expectation <- array(0, dim = c(tt, dd))
-  yy_predict_variance <- yy_cov + zz[1:dd, ] %*% beta_update_variance %*%
-    t(zz[1:dd, ])
+  yy_predict_variance <- yy_cov + zz[1:dd, ] %*%
+    beta_update_variance %*% t(zz[1:dd, ])
   yy_update_variance <- kk * yy_cov
 
   list(
-    dd = dd, hh = hh, pp = pp, tt = tt, yy = yy, yraw = yraw, zz = zz,
-    zz_cols_number = zz_cols_number, zz_rows_number = zz_rows_number,
+    dd = dd, hh = hh, pp = pp,
+    tt = tt, yy = yy, yraw = yraw, zz = zz,
+    zz_cols_number = zz_cols_number,
+    zz_rows_number = zz_rows_number,
     beta_predict_expectation = beta_predict_expectation,
     beta_predict_variance = beta_predict_variance,
     beta_update_expectation = beta_update_expectation,
