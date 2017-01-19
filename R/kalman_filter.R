0383efa6f560a8772e83839bf02af35ac2db7eb7
Weigt <t_weig05@WIWI.UNI-MUENSTER.DE>
2017-01-19 09:36:47 +0100

    - minor changes to model_initialize - kalman_filter ready implemented - dms still missing

--- R/kalman_filter.R
+++ R/kalman_filter.R
@@ -2,14 +2,20 @@ kalman_filter <- function(pp = 1, hh = 1,
                           prior_constant_variance = 10,
                           gg = .1, kk = 1, ll = 1) { # function(yraw, pp, hh, prior_constant_variance, gg, kk, ll)
 
-# generate the data ----------------------------------------
-yraw <- cbind(1:3, 11:13)
+# only temporary for developemnet
+yraw <- cbind(1:10, 11:20)
+prior_constant_variance = 10
+gg = .1
+kk = 1
+ll = 1
+t = 2
 
-# initialize the model -------------------------------------
+# initialize the model ----------------------------------------
 model <- model_initialize(yraw, pp, hh,
   prior_constant_variance, gg, kk, ll)
 
-# assign each object in the model list to a matrix
+# assign each object in the model list to a matrix -----------
+{
 dd <- model$dd
 hh <- model$hh
 pp <- model$pp
@@ -25,13 +31,63 @@ beta_update_expectation <- model$beta_update_expectation
 beta_update_variance <- model$beta_update_variance
 yy_predict_expectation <- model$yy_predict_expectation
 yy_predict_variance <- model$yy_predict_variance
+yy_predict_variance_inverse <-
+  model$yy_predict_variance_inverse
+yy_predict_error_expectation <-
+  model$yy_predict_error_expectation
+yy_predict_error_variance <- model$yy_predict_error_variance
 yy_update_variance <- model$yy_update_variance
-
-for (t in 1:tt) {
-
 }
 
-return(model)
+# kalman filter -----------------------------------------------
+# progressbar <- utils::txtProgressBar(min = 0, max = tt,
+# style = 3)
+for (t in 2:tt) {
+  # utils::setTxtProgressBar(pb = progressbar, value = t)
+  # shift set minus 1 due to the prior at point in time 1
+  zz_t_index <- (t * dd - dd + 1):(t * dd)
+  # beta prediction
+  beta_predict_expectation[t, ] <-
+    beta_update_expectation[t - 1, ]
+  beta_predict_variance <- beta_update_variance / ll
+  # yy prediction
+  yy_predict_expectation[t, ] <- zz[zz_t_index, ] %*%
+    beta_predict_expectation[t, ]
+  yy_predict_error_expectation[t, ] <-
+    yy[t, ] - yy_predict_error_expectation[t, ]
+  yy_predict_error_variance <-
+    yy_predict_error_expectation[t, ] %*%
+    t(yy_predict_error_expectation[t, ])
+  # yy_update_variance
+  if (kk == 0) {
+    # raftery setting
+    temp <- yy_update_variance
+    yy_update_variance <- yy_update_variance / ((t - 1) / t) +
+      (yy_predict_error_variance - zz[zz_t_index, ] %*%
+      beta_predict_variance %*% t(zz[zz_t_index, ])) / t
+    if (any(eigen(yy_update_variance)$value) <= 0) {
+      yy_update_variance <- temp
+    }
+  } else {
+    # korobilis setting
+    yy_update_variance <- yy_update_variance / kk +
+      yy_predict_error_variance / (1 - kk)
+  }
+  yy_predict_variance <- zz[zz_t_index, ] %*%
+    beta_predict_variance %*% t(zz[zz_t_index, ]) +
+    yy_update_variance
+  # beta_update
+  yy_predict_variance_inverse <- solve(yy_predict_variance)
+  beta_update_expectation[t, ] <-
+    beta_predict_expectation[t, ] + beta_predict_variance %*%
+    t(zz[zz_t_index, ]) %*% yy_predict_variance_inverse %*%
+    yy_predict_error_expectation[t, ]
+  beta_update_variance <- beta_predict_variance -
+    beta_predict_variance %*% t(zz[zz_t_index, ]) %*%
+    yy_predict_variance_inverse %*% zz[zz_t_index, ] %*%
+    beta_predict_variance
+}
+# close(pb)
 
 # # build model_probability_predict and
 # # model_probability_update
@@ -39,4 +95,6 @@ return(model)
 #   gg_length * kk_length *
 #     ll_length * ff_length))
 # model_probability_update <- model_probability_predict
+
+return(model)
 }
--- R/model_initialize.R
+++ R/model_initialize.R
@@ -4,31 +4,37 @@ model_initialize <- function(yraw, pp, hh,
                              gg, kk, ll) {
 
   # build the variables of the model
-  dd <- dim(yraw)[2] # dimension of the time series
-  yy <- yraw[(pp + hh):nrow(yraw), ] # time series shifted due
-                                     # to pp and hh
-  tt <- dim(yy)[1] # length of the time series
-  zz <- zz_build(yraw, pp, hh, dd, tt) # build the variable zz
-  zz_cols_number <- dim(zz)[1] # number of rows of zz
-  zz_rows_number <- dim(zz)[2] # number of cols of zz
+  dd <- dim(yraw)[2]  # dimension of the time series
+  yy <- yraw[(pp + hh):nrow(yraw), ]  # time series shifted due
+                                      # to pp and hh
+  yy_cov <- stats::cov(yy)
+  tt <- dim(yy)[1]  # length of the time series
+  zz <- zz_build(yraw, pp, hh, dd, tt)  # build the variable zz
+  zz_rows_number <- dim(zz)[1]  # number of rows of zz
+  zz_cols_number <- dim(zz)[2]  # number of cols of zz
+  # add unused NA rows for the prior at point in time 1
+  yy <- rbind(rep(NA, dd), yy)
+  zz <- rbind(matrix(NA, nrow = dd, ncol = zz_cols_number), zz)
 
   # build beta_predict_expectation and beta_update_variance
-  tt <- tt + 1 # the first entry is reserved for the prior
-               # at point in time 0
   beta_update_expectation <- array(0, dim = c(tt,
-    zz_rows_number))
-  beta_predict_expectation <- beta_update_expectation
+    zz_cols_number))
+  beta_predict_expectation <- NA * beta_update_expectation
 
   # build beta_predict_variance and beta_update_variance
   beta_update_variance <- diag(c(rep(prior_constant_variance, dd),
     rep(rep(1, dd), dd)))
-  beta_predict_variance <- beta_update_variance
+  beta_predict_variance <- NA * beta_update_variance
 
-  # build yy_predict_expectation, yy_predict_variance and yy_update_variance
-  yy_cov <- stats::cov(yy)
-  yy_predict_expectation <- array(0, dim = c(tt, dd))
+  # build yy_predict_expectation, yy_predict_variance,
+  # yy_update_variance, yy_predict_error_expectation,
+  # yy_predict_error_variance, yy_predict_variance_inverse
+  yy_predict_expectation <- array(NA, dim = c(tt, dd))
   yy_predict_variance <- yy_cov + zz[1:dd, ] %*%
     beta_update_variance %*% t(zz[1:dd, ])
+  yy_predict_variance_inverse <- NA * yy_predict_variance
+  yy_predict_error_expectation <- yy_predict_expectation
+  yy_predict_error_variance <- array(NA, dim = c(dd, dd))
   yy_update_variance <- kk * yy_cov
 
   list(
@@ -42,6 +48,10 @@ model_initialize <- function(yraw, pp, hh,
     beta_update_variance = beta_update_variance,
     yy_predict_expectation = yy_predict_expectation,
     yy_predict_variance = yy_predict_variance,
+    yy_predict_variance_inverse = yy_predict_variance_inverse,
+    yy_predict_error_expectation =
+      yy_predict_error_expectation,
+    yy_predict_error_variance = yy_predict_error_variance,
     yy_update_variance = yy_update_variance
   )
 }
