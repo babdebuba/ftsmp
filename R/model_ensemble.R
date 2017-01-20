a38ab5ce263aedb514a912adc003649504478f90
Weigt <t_weig05@WIWI.UNI-MUENSTER.DE>
2017-01-20 11:46:48 +0100

    testing without import(foreach)

--- R/model_ensemble.R
+++ R/model_ensemble.R
@@ -1,25 +1,25 @@
 #' @export
-#' @import foreach
-model_ensemble <- function(gg = .1, kk = 1, ll = 1) {
+model_ensemble <- function(gg = .1, kk = 1, ll = 1,
+  cores_number = 1) {
 
   # only temporary for developemnet !!!!!!!!!!!!!!!!!!!!!!!!!!
   # yraw <- cbind(1:3, 11:13) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  yraw <- MTS::VARMAsim(nobs = 100, arlags = 1,
+  yraw <- MTS::VARMAsim(nobs = 1000, arlags = 1,
     phi = matrix(c(0.2, -0.6, 0.3, 1), 2, 2),
     sigma = matrix(c(4, 0.8, 0.8, 1), 2, 2))$series
-  gg <- c(.1, .2)
-  kk <- c(1, 2)
-  ll <- c(1, 2)
+  gg <- seq(.001, .01, length.out = 5)
+  kk <- seq(.8, 1, length.out = 5)
+  ll <- seq(.9, 1, length.out = 5)
 
   model_parameter <- expand.grid(gg = gg, kk = kk, ll = kk)
 
   models <- rep(list(""), dim(model_parameter)[1])
-  for (i in 1:8) {
+  for (i in 1:dim(model_parameter)[1]) {
     models[[i]] <- list(yraw, gg = model_parameter[i, 1],
       kk = model_parameter[i, 2], ll = model_parameter[i, 3])
   }
 
-  cl <- parallel::makeCluster(2)
+  cl <- parallel::makeCluster(cores_number)
   doParallel::registerDoParallel(cl)
 
   models <- foreach::foreach(i = 1:length(models),
