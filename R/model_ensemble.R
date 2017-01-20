7325479e4d90b11f06365e448009cadac36664f2
Weigt <t_weig05@WIWI.UNI-MUENSTER.DE>
2017-01-20 12:08:32 +0100

    - added documentation to model-ensemble - cran check ok

--- R/model_ensemble.R
+++ R/model_ensemble.R
@@ -1,5 +1,11 @@
 #' @export
 #' @import foreach
+#' @param gg prior parameter
+#' @param kk observe variance parameter
+#' @param ll state variance parameter
+#' @param cores_number define the number of cores
+#' @examples model_ensemble()
+#' @return the return
 model_ensemble <- function(gg = .1, kk = 1, ll = 1,
   cores_number = 1) {
 
@@ -8,9 +14,9 @@ model_ensemble <- function(gg = .1, kk = 1, ll = 1,
   yraw <- MTS::VARMAsim(nobs = 1000, arlags = 1,
     phi = matrix(c(0.2, -0.6, 0.3, 1), 2, 2),
     sigma = matrix(c(4, 0.8, 0.8, 1), 2, 2))$series
-  gg <- seq(.001, .01, length.out = 5)
-  kk <- seq(.8, 1, length.out = 5)
-  ll <- seq(.9, 1, length.out = 5)
+  gg <- seq(.001, .01, length.out = 2)
+  kk <- seq(.8, 1, length.out = 2)
+  ll <- seq(.9, 1, length.out = 2)
 
   model_parameter <- expand.grid(gg = gg, kk = kk, ll = kk)
 
